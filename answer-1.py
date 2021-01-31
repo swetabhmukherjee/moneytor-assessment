694b4374c1862c3b4806e992e60479a49b1a09ca
Swetabh Mukherjee <swetabh.libra@gmail.com>
2021-01-31 18:10:53 +0530

    added constraints

--- answer-1.py
+++ answer-1.py
@@ -1,12 +1,15 @@
 # function to implement exponent
 
 def exponent(base,exp):
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return(base*exponent(base,exp-1))
+    if((base>=1) and (exp<=10)):
+        if(exp==1):
+            return(base)
+        if(exp!=1):
+            return(base*exponent(base,exp-1))
+    else:
+        return "out of bounds"
 
 # driver function
 
-a = exponent(5,3)
+a = exponent(5,11)
 print(a)
